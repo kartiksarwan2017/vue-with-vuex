d0212285d9c93baabe11b1527d74e62af8d97730
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-02-26 09:42:33 +0530

    Actions in VueX

--- src/components/VueCounter.vue
+++ src/components/VueCounter.vue
@@ -2,18 +2,21 @@
     <div>
         <h1>Vuex Counter</h1>
         <h1 class="counter">{{ counter }}</h1>
-        <button>-</button>
+        <button @click="subtractFromCounter(parseInt(value))">-</button>
         <input 
             type="number" 
             v-model="value"
         />
-        <button>+</button>
+        <button @click="addToCounter(parseInt(value))">+</button>
+        <div>
+          <button class="btn" @click="addRandomNumber">Add By Random Number</button>
+        </div>
     </div>
 </template>
 
 <script>
 
-import { mapState } from "vuex";
+import { mapState, mapMutations, mapActions } from "vuex";
 
 export default {
     data() {
@@ -23,6 +26,10 @@ export default {
     },
     computed: {
       ...mapState(["counter"])
+    },
+    methods: {
+      ...mapMutations(['addToCounter', 'subtractFromCounter']),
+      ...mapActions(['addRandomNumber'])
     }
 };
 </script>
@@ -43,5 +50,13 @@ export default {
     padding: 0.4rem;
     margin: 0 0.5rem;
     text-align: center;
+  }
+  .btn {
+    border-radius: 0.5rem;
+    width: auto;
+    background-color: #41B983;
+    margin-top: 1rem;
+    color: white;
+    cursor: pointer;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,28 @@
 import { createApp } from 'vue';
 import { createStore } from "vuex";
 import App from './App.vue';
+import axios from "axios";
 
 const store = createStore({
     state() {
         return {
             counter: 10
+        }
+    },
+    mutations: {
+        addToCounter(state, payload) {
+            state.counter = state.counter + payload;
+        },
+        subtractFromCounter(state, payload) {
+            state.counter = state.counter - payload;
+        }
+    },
+    actions: {
+        async addRandomNumber(context) {
+            // API will returnt the randome number between [-1000, 1000]
+            let data = await axios.get("https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new");
+            // console.log(data);
+            context.commit("addToCounter", data.data);
         }
     }
 });
